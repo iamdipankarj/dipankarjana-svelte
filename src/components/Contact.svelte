81351d02c56be22e349b73c85647f6423182c4a4
Dipankar Jana <dipankar@okcredit.in>
2025-04-09 14:55:37 +0530

    Add linkedin to contact section

--- src/components/Contact.svelte
+++ src/components/Contact.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import EmailIcon from '../assets/email.svg'
   import LinkIcon from '../assets/website.svg'
+  import LinkedInIcon from '../assets/linkedin-alt.svg'
   import PhoneIcon from '../assets/phone.svg'
   import LocationIcon from '../assets/location.svg'
 </script>
@@ -24,6 +25,15 @@
       <div class="flex flex-col flex-1 gap-1">
         <h4 class="text-sm tracking-wide text-social">Website</h4>
         <p class="text-socialtext font-semibold tracking-wide hover:underline">https://dipankarjana.com</p>
+      </div>
+    </a>
+    <a class="flex items-center gap-3" href="https://linkedin.com/in/iamdipankarj" rel="nofollow" target="_blank">
+      <div class="w-10 h-10 bg-socialicon rounded-full flex items-center justify-center shrink-0">
+        <img src={LinkedInIcon} alt="Education" class="block w-4 h-4 shrink-0" />
+      </div>
+      <div class="flex flex-col flex-1 gap-1">
+        <h4 class="text-sm tracking-wide text-social">LinkedIn</h4>
+        <p class="text-socialtext font-semibold tracking-wide hover:underline">https://linkedin.com/in/iamdipankarj</p>
       </div>
     </a>
     <a class="flex items-center gap-3" href="tel:9674878096" rel="nofollow" target="_blank">
